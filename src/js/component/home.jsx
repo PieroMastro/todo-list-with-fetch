b6e9ff764aa7d1c156b91052b2860aac92a5c3c0
PieroMastro <pier.andres@gmail.com>
2022-10-02 07:43:27 +0000

    Everything working, CSS complete

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,53 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { element } from "prop-types";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+
+	const [inputValue, setInputValue] = useState("")
+	const [list, setList] = useState([]);
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="container rounded">
+			<h1 className="header display-1 mb-2 mt-5">todos</h1 >
+			<div className="form shadow mb-5 bg-body rounded">
+				<card className="list">
+					<input
+						className="input"
+						type="text"
+						onKeyDown={(event) => {
+							if (event.key === "Enter" && inputValue.trim() !== "") {
+								setList([...list, inputValue])
+								setInputValue("")
+							}
+						}}
+						onChange={(event) => setInputValue(event.target.value)}
+						value={inputValue}
+						placeholder="Add a New Task!" >
+					</input>
+				</card>
+				<div className="list">
+					{list.map((elementList, index) => {
+						return <li key={index} className="todolist d-flex justify-content-between">
+							{elementList}
+							<button
+								className="button d-flex"
+								type="button"
+								onClick={(event) => setList(list.filter((element, id) => { return index !== id }))
+								}><i class="fa-regular fa-circle-xmark"></i></button>
+						</li>
+					})
+					}
+					<span className="tasks d-flex">{
+						list.length === 0 ? "No Tasks, add a Task!" :
+							list.length === 1 ? list.length + " Task Left" :
+								list.length + " Tasks Left"
+					}
+					</span>
+				</div>
+			</div>
+		</div >
 	);
 };
 
