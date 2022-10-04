7acc7590c86b767ba9936ce33566d38d8d9ece9e
PieroMastro <pier.andres@gmail.com>
2022-10-04 19:24:20 +0000

    GET POST done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,41 +1,83 @@
 import { element } from "prop-types";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 const Home = () => {
 
-	const [inputValue, setInputValue] = useState("")
+	let initialState = { label: "", done: false }
+	const [inputValue, setInputValue] = useState(initialState)
 	const [list, setList] = useState([]);
 
+	const handleChange = (event) => {
+		setInputValue({
+			...inputValue,
+			[event.target.name]: event.target.value
+		})
+	}
+
+	const getTasks = async () => {
+		try {
+			let response = await fetch(`https://assets.breatheco.de/apis/fake/todos/user/pieromastro`)
+			let data = await response.json()
+			console.log(response.status);
+			if (response.status == 404) {
+				createTodos();
+			}
+			else {
+				setList(data)
+			}
+		} catch (error) {
+			//En caso de que ocurra un error
+			console.log(error);
+		}
+	}
+	const createTodos = async () => {
+		try {
+			let response = await fetch(`https://assets.breatheco.de/apis/fake/todos/user/pieromastro`, {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify([])
+			})
+			let data = await response.json()
+			if (response.ok) {
+				getTasks();
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
+	useEffect(() => { getTasks() }, [])
 
 	return (
 		<div className="container rounded">
 			<h1 className="header display-1 mb-2 mt-5">todos</h1 >
 			<div className="form shadow mb-5 bg-body rounded">
-				<card className="list">
+				<div className="list">
 					<input
 						className="input"
 						type="text"
+						name="label"
 						onKeyDown={(event) => {
-							if (event.key === "Enter" && inputValue.trim() !== "") {
+							if (event.key === "Enter" && inputValue.label.trim() !== "") {
 								setList([...list, inputValue])
-								setInputValue("")
+								setInputValue(initialState)
 							}
 						}}
-						onChange={(event) => setInputValue(event.target.value)}
-						value={inputValue}
+						// onChange={(event) => setInputValue(event.target.value)}
+						onChange={handleChange}
+						value={inputValue.label}
 						placeholder="Add a New Task!" >
 					</input>
-				</card>
+				</div>
 				<div className="list">
 					{list.map((elementList, index) => {
 						return <li key={index} className="todolist d-flex justify-content-between">
-							{elementList}
+							{elementList.label}
 							<button
 								className="button d-flex"
 								type="button"
 								onClick={(event) => setList(list.filter((element, id) => { return index !== id }))
-								}><i class="fa-regular fa-circle-xmark"></i></button>
+								}><i className="fa-regular fa-circle-xmark"></i></button>
 						</li>
 					})
 					}
